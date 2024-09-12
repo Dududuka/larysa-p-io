ac4fed91e9378951741a8d02a3de585e3ec95700
Larysa <izumrud@gmail.com>
2024-09-12 00:55:29 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -17,3 +17,37 @@ for (let i = 0; i < skills.length; i++) {
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.forms['leave_message'];
+messageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log("Name: ", usersName);
+    console.log("Email: ", usersEmail);
+    console.log("Message: ", usersMessage);
+
+    const messageSection = document.getElementById('messages');
+
+    let messageList = messageSection.querySelector('ul');
+
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> <span> - ${usersMessage}</span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', function() {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    })
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    event.target.reset();
+})
